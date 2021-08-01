6930448de945413de0beb0f9c297fbf589a8a699
Mohammadreza <rezalahmi@gmail.com>
2021-08-01 05:42:32 +0430

    Create NumericOperation.py

---
+++ NumericOperation.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#multiplication
+5*5
+
+
+# In[3]:
+
+
+#addition
+5+5
+
+
+# In[5]:
+
+
+#subtraction
+5-5
+
+
+# In[6]:
+
+
+#division remaining
+5%5
+
+
+# In[8]:
+
+
+#division
+5/5
+
+
+# In[9]:
+
+
+#powers
+5**5
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
