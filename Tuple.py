7abd466a7e0f16c73561f59a9fe80f3259966ebd
Mohammadreza <rezalahmi@gmail.com>
2021-08-04 05:33:59 +0430

    Update Tuple.py

--- Tuple.py
+++ Tuple.py
@@ -80,6 +80,76 @@ tuple_c[::2]
 len(tuple_c)
 
 
+# In[14]:
+
+
+print(tuple_c[-1])
+#Negative indexing means start from the end.
+print(tuple_c[-2])
+
+
+# In[17]:
+
+
+if "ali" in tuple_c:
+    print("yes")
+
+
+# In[18]:
+
+
+if "reza" in tuple_c:
+    print("yes")
+
+
+# In[19]:
+
+
+#The del keyword can delete the tuple completely
+del tuple_a
+
+
+# In[20]:
+
+
+print(tuple_a)
+
+
+# In[21]:
+
+
+#unpacking
+#When we create a tuple, we normally assign values to it. This is called "packing" a tuple
+#But, in Python, we are also allowed to extract the values back into variables.
+#This is called "unpacking"
+(red,blue) = tuple_b
+print(red)
+print(blue)
+
+
+# In[22]:
+
+
+#If you want to multiply the content of a tuple a given number of times, you can use the * operator
+tuple_b2 = tuple_b * 2
+print(tuple_b2)
+
+
+# In[24]:
+
+
+#The count() method returns the number of times a specified value appears in the tuple.
+tuple_b2.count("eli")
+
+
+# In[26]:
+
+
+#The index() method finds the first occurrence of the specified value
+print(tuple_c.index(59))
+#if item not found in the tuple, exception occure
+
+
 # In[ ]:
 
 
